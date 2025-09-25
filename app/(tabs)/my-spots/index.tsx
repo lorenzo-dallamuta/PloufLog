c267a67e35666c8a5fe04df94ac492f5ca1edd0f
Lorenzo Dalla Muta <lorenzodallamuta@gmail.com>
2025-09-25 18:13:57 +0200

    spots, implement ui component to satisfy the loader indicator for the dive spots list (careful: the accessibility prop on rn built components must be declared explicitely)

--- app/(tabs)/my-spots/index.tsx
+++ app/(tabs)/my-spots/index.tsx
@@ -1,14 +1,27 @@
-import { StyleSheet } from 'react-native';
+import { ActivityIndicator, StyleSheet } from 'react-native';
 
 import { Text, View } from '@/src/components/Themed';
+import { useDiveSiteIsLoading } from '@/src/store/useDiveSiteStore';
 
 export default function SpotListScreen() {
+  const diveSiteIsLoading = useDiveSiteIsLoading();
+
+  if (diveSiteIsLoading) {
+    return (
+      <ActivityIndicator 
+        accessibilityHint='loading-spinner' 
+        role='progressbar' 
+        accessible
+      />
+    );
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>My Spots List - Coming Soon</Text>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
