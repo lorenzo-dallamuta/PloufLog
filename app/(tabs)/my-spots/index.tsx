78d91253b591f4f1bd202295578767a6319072f8
Lorenzo Dalla Muta <lorenzodallamuta@gmail.com>
2025-09-25 18:58:37 +0200

    testing, implement ui view to satisfy the tests of the empty state of the SpotsListScreen component

--- app/(tabs)/my-spots/index.tsx
+++ app/(tabs)/my-spots/index.tsx
@@ -1,20 +1,37 @@
 import { ActivityIndicator, StyleSheet } from 'react-native';
 
 import { Text, View } from '@/src/components/Themed';
-import { useDiveSiteIsLoading } from '@/src/store/useDiveSiteStore';
+import { useDiveSiteIsLoading, useDiveSiteList } from '@/src/store/useDiveSiteStore';
 
 export default function SpotListScreen() {
+  const diveSiteList = useDiveSiteList();
   const diveSiteIsLoading = useDiveSiteIsLoading();
 
   if (diveSiteIsLoading) {
     return (
-      <ActivityIndicator 
-        accessibilityHint='loading-spinner' 
-        role='progressbar' 
+      <ActivityIndicator
+        accessibilityHint='loading-spinner'
+        role='progressbar'
         accessible
       />
     );
   };
+
+  if (diveSiteList.length === 0) {
+    return (
+      <View style={styles.container}>
+        <Text
+          testID='empty-list'
+          accessibilityLabel='No dive spots'
+          accessibilityRole='text'
+          accessible
+          style={styles.title}
+        >
+          Add a dive spot to see it in your list
+        </Text>
+      </View>
+    )
+  }
 
   return (
     <View style={styles.container}>
